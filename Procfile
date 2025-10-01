fab9427b0ca19861853dc5239fe8c638af10b82f
Sarif Mia <sarifmia.ofc@gmail.com>
2025-10-01 16:25:37 +0600

    first commit

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: cd backend && python manage.py migrate
-web: cd backend && gunicorn agency_backend.wsgi:application --bind 0.0.0.0:$PORT
--- backend/start_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "D:\Project\AGENCY\backend"
-set DJANGO_SETTINGS_MODULE=agency_backend.settings
-call ..\venv\Scripts\activate.bat
-python manage.py migrate
-python manage.py runserver 8000
-pause
